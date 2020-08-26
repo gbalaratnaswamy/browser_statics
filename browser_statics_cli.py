9ba1d2bb6050af3825f0b1424aff774d24619e13
gbalaratnaswamy <gbalaratnaswamy@gmail.com>
2020-08-26 21:54:00 +0530

    initial commit

---
+++ browser_statics_cli.py
@@ -0,0 +1,41 @@
+# this is a command line app to fetch and plot data
+# 
+
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import sys
+from cfg import *
+from main import extract_data
+from plot_data import plot_data
+arguments=sys.argv
+file_path=arguments[0]
+
+# get command
+try :
+    command=arguments[1]
+# if command not given
+except:
+    print("error occured")
+    sys.exit()
+
+# if user requests to fetch data from w3schools
+if command=="fetch":
+    db=extract_data()
+    db.to_csv(FILE_NAME,index=False)
+    print("fetch sucess")
+    print(db)
+elif command=="set":
+    pass
+elif command=="get":
+    pass
+elif command=="plot_data":
+    try:
+        plot_data(arguments[2])
+    except:
+        print("invalid browser name try one of these")
+        db=pd.read_csv(FILE_NAME)
+        for col in db.columns[2:]:
+            print(col)
+
+
--- plot_data.py
+++ plot_data.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from cfg import FILE_NAME
 
-db=pd.read_csv(FILE_NAME)
 
 # function to plot data
 def plot_data(browser_name):
+    db=pd.read_csv(FILE_NAME)
     data=db[browser_name].to_numpy(dtype=float)
     plt.plot(data)
     plt.show()
 
-plot_data("Opera")
+if __name__ == "__main__":
+    plot_data("Opera")
